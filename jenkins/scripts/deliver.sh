399dc660ea378214a26e3ef90a08547447e1bcda
jcabelloc <jcabelloc@itana.pe>
2020-10-17 16:20:22 -0500

    Print version de Java

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -20,7 +20,12 @@ set -x
 VERSION=`mvn help:evaluate -Dexpression=project.version | grep "^[^\[]"`
 set +x
 
+echo 'Java version'
+java -version
+
+
 echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
 set -x
 java -jar target/${NAME}-${VERSION}.jar
+
